34e6e3898fb7f360c9086f840004d05b70c350c0
Roy Contreras Benitez <roy7474@gmail.com>
2024-02-05 19:14:35 -0500

    class defined for the game. Screen size has been set

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,2 +1,15 @@
 import sys
 import pygame
+
+class AlienInvasion:
+    '''Overall class to manage game assets and behavior'''
+
+    def _init(self):
+        '''initialize the game, and create game resources'''
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        ''' Start the main loop for the game'''
