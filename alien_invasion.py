02ea9e0704606b5514536283dafd19ce6c5db0a6
Roy Contreras Benitez <roy7474@gmail.com>
2024-02-05 19:18:33 -0500

    class and parameters have been stablished

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,3 +13,12 @@ class AlienInvasion:
 
     def run_game(self):
         ''' Start the main loop for the game'''
+        while True:
+            #watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type ==pygame.QUIT:
+                    sys.exit()
+            
+            #Make the most recently drawn screen visible.
+            pygame.display.flip()
+
